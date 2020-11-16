892d5bba34952d49aac164c5f5381df820850b81
gambit807 <74557264+gambit807@users.noreply.github.com>
2020-11-16 20:07:59 +0200

    Delete errors.c

--- errors.c
+++
@@ -1,8 +0,0 @@
-#include "assembler.h"
-
-int print_error(char *string)
-{
-	/*prints a detailed error message and returns FALSE*/
-	printf("ERROR - file %s, line %d : %s\n",file_name, line_number,string);
-	return FALSE; 
-}
